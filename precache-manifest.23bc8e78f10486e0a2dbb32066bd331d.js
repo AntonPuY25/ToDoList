a74a21299a950b88757fb38c68aa176f6c99b0d7
AntonPuY25 <puy25@bk.ru>
2021-01-08 22:07:40 +0300

    Updates

---
+++ precache-manifest.23bc8e78f10486e0a2dbb32066bd331d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618fea843f41f773403ceb086f739aa1",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "ceefe576c0f7c2cde004",
+    "url": "/ToDoList/static/css/main.c4b54734.chunk.css"
+  },
+  {
+    "revision": "ec7f3cd645a4c875793c",
+    "url": "/ToDoList/static/js/2.f3892931.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ToDoList/static/js/2.f3892931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceefe576c0f7c2cde004",
+    "url": "/ToDoList/static/js/main.7d7a186b.chunk.js"
+  },
+  {
+    "revision": "448f1ac8991118963dc3",
+    "url": "/ToDoList/static/js/runtime-main.97949b04.js"
+  }
+]);
